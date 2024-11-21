b9a750ea4c7867d4dc1da266216e89434e52b555
LastProgram <145275888+LastProgram@users.noreply.github.com>
2024-11-21 15:44:20 +0700

    Edit MainActivity output

--- app/src/main/java/com/example/android_lab3/MainActivity.kt
+++ app/src/main/java/com/example/android_lab3/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.android_lab3
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,10 +11,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val editText = findViewById<EditText>(R.id.edit_text)
+        val textView = findViewById<TextView>(R.id.text_view)
 
         editText.setOnEditorActionListener{ _, _, _ ->
             var input = editText.text.toString()
-
+            var output = countLatinLetter(input)
+            textView.text = output.toString()
             true
         }
     }
