6a38dbd26b129e220bd728882d5932681848187e
LastProgram <145275888+LastProgram@users.noreply.github.com>
2024-11-21 15:39:56 +0700

    Edit MainActivity

--- app/src/main/java/com/example/android_lab3/MainActivity.kt
+++ app/src/main/java/com/example/android_lab3/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.android_lab3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val editText = findViewById<EditText>(R.id.edit_text)
+
+        editText.setOnEditorActionListener{ _, _, _ ->
+            var input = editText.text.toString()
+
+            true
+        }
     }
 }
